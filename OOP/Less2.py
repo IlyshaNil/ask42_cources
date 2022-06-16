874fca38725bcc5f81203907e8b34a77549e4362
IlyshaNil <hacharoky86@gmail.com>
2022-06-16 17:14:06 +0300

    add: oop-les2

---
+++ OOP/Less2.py
@@ -0,0 +1,35 @@
+import random
+
+
+class Warrior:
+    name = "default_name"
+    health = 100
+    damage = 20
+
+    def attack(self, aim):
+        print(f'{self.name} attack!')
+        aim.getDamage(self.damage)
+        
+
+    def getDamage(self, damage):
+        self.health -= damage
+        print(f'{self.health} HP left with {self.name}\n')
+
+        if self.health <= 0:
+            print(f'{self.name} lose :(')
+
+
+saitama = Warrior()
+saitama.name = "saitama"
+
+default_warrior = Warrior()
+default_warrior.name = "default_warrior"
+
+while (saitama.health > 0 and default_warrior.health > 0):
+    initiator = random.choice((saitama, default_warrior))
+    if initiator is saitama:
+        saitama.attack(default_warrior)
+    else:
+        default_warrior.attack(saitama)    
+
+print(f'saitama win!') if default_warrior.health <= 0 else print(f'default_warrior win!')
